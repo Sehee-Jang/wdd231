839ac637a84b3517ac89bd05aa78ea96ba2859d3
Sehee Jang <seheejang94@gmail.com>
2024-10-27 12:38:57 +0900

    update: top function

--- cinequest/js/top.js
+++ cinequest/js/top.js
@@ -1,7 +1,18 @@
 // Top Button
-document.getElementById("top-btn").addEventListener("click", function () {
+// document.getElementById("top-btn").addEventListener("click", function () {
+//   window.scroll({
+//     behavior: "smooth",
+//     top: 0,
+//   });
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("top-btn").addEventListener("click", toTop);
+});
+
+function toTop() {
   window.scroll({
     behavior: "smooth",
     top: 0,
   });
-});
+}
